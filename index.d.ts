fc6b149b671ee26581566e002cbfd118ce335148
rodion <rodionaaafgh@gmail.com>
2018-12-27 19:40:52 +0300

    update: file with declarations

---
+++ index.d.ts
@@ -0,0 +1,18 @@
+import * as React from "react"
+
+export interface Theme {
+  actionColor: string;
+  selectColor: string;
+  selectTextColor: string;
+  circleColor: string;
+}
+
+export interface Props {
+  duration: number;
+  wrapper: React.Component;
+  onStart: () => void;
+  onDurationChange: (duration: number) => void;
+  theme: Theme
+}
+
+declare class TimePicker extends React.Component<SimpleSelectProps, any> {}
